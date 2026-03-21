17d2daa9960d513c0cb2c73a79f2a8b86c10c750
yun29-maker <yun.29eme.district@gmail.com>
2026-03-21 21:21:46 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>NotePAC</h1>
+      <h1>NoteVase</h1>
       <p>
         Faites votre étude gratuitement, visualisez le résultat immédiatement,
         puis payez seulement 2,50 € pour obtenir le PDF.
